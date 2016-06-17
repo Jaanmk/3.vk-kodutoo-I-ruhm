945debfabee4c6f83edb4ef50a58004334753f47
Jaan-Martin Kuusmann <jaan@araneus.ee>
2016-06-17 11:39:23 +0300

    Katkine

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ var laheKood = {
     laadimine: function(){
         $('<input/>').attr({type:'text', id: 'smth'}).appendTo('.input');
         $('<button>').attr({type:'button', id:'save'}).html('salvesta').appendTo('.input');
-        this.list = laheKood.stringify();
-        for (i = 0; i < this.list.length; i++) {
-            this.looList(this.list[i].id, this.list[i].value);
-        }
-        laheKood.idCount = this.list.length;
-        laheKood.list = this.list;
+
+        //console.log(this.list);
+        for (var i = 0; i < this.list.length; i++) {
+                //console.log(laheKood.list[i]);
+                laheKood.looList(this.list[i].id,this.list[i].value);
+            }
 
     },
 
     salvestamine: function(){
-        if (window.console) console.log('sisestus');
         $('<p>').attr({class: 'miskit'}).html($("#smth").val()).appendTo('.content');
         console.log(laheKood.idCount);
         var id = laheKood.list.length ++;
@@ -26,15 +25,16 @@ var laheKood = {
         });
 
         laheKood.kirjutaTxt(laheKood.list);
-
+        console.log(this.list);
     },
 
     looList: function(id, value){
         $('<p>').attr({class: 'miskit', id: id}).html($("#smth").val()).appendTo('.content');
-
+        console.log(id +' ' +value);
     },
 
     kirjutaTxt: function(list){
+        console.log(list);
         var data = new FormData();
         data.append("data" , JSON.stringify(list));
         var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
@@ -43,18 +43,52 @@ var laheKood = {
     },
 
     loeTxt: function(){
-        var andmed;
-        $.ajax({
+        /* var andmed;
+        return $.ajax({
            url : "saved/smth.txt",
            dataType: "text",
-           success : function (data) {
+          success : function (data) {
                $(".text").html(data);
                this.andmed = data;
                console.log(this.andmed);
+
            }
 
 
-       });
+       });*/
+       $.ajax({
+        url: "saved/smth.txt",
+        async: true,   // asynchronous request? (synchronous requests are discouraged...)
+        cache: true,   // with this, you can force the browser to not make cache of the retrieved data
+        dataType: "text",  // jQuery will infer this, but you can set explicitly
+        success: function( data, textStatus, jqXHR ) {
+            this.resourceContent = JSON.parse(data); // can be a global variable too...
+            // process the content...
+            this.list = JSON.parse(data);
+            console.log(this.list.length);
+            var pikkus = this.list.length;
+
+            for (var i = 0; i < pikkus; i++) {
+                    if (this.list[i]===null){
+                        delete this.list[i];
+                        //console.log(i+'spliced');
+
+
+                    }else{
+                        //delete this.list[i];
+                        laheKood.list.push(this.list[i]);
+                        //console.log('salvestan listi'+ i);
+
+                    }
+
+                }
+            laheKood.laadimine();
+
+            }
+
+
+        });
+
    },
 
    stringify: function(){
@@ -64,6 +98,6 @@ var laheKood = {
    }
 };
 $( document ).ready( function(){
-    laheKood.laadimine();
-    $( "#save" ).click( laheKood.salvestamine );
+    laheKood.loeTxt();
+    $( "#save" ).click( laheKood.salvestamine() );
 });
